2353337fce22594320da0f892e1767fc8b7465c8
DevRayyan <rayyanahmed1624@gmail.com>
2025-06-27 12:29:21 +0500

    Updated Class_Model.py

--- Class_Models.py
+++ Class_Models.py
@@ -44,11 +44,11 @@ def get_db_connection():
     try:
         load_dotenv()
         conn = mysql.connector.connect(
-            host=os.getenv("DB_HOST", "localhost"),
-            user=os.getenv("DB_USER", "root"),
-            password=os.getenv("DB_PASSWORD", "cs138"),
-            database=os.getenv("DB_NAME", "bluewheelsdb"),
-            port=int(os.getenv("DB_PORT", 3306))
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT"))
         )
         
         if conn.is_connected():
